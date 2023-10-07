ed7f57310f156d358166d1ed5150940589527338
Collins-cl <collinsolads@gmail.com>
2023-10-07 16:07:16 +0100

    hero construct

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,34 +8,36 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="wrapper">
-        <div className="top">
-          <div className="intro">Get more than a reliable power plant</div>
+        <div className="div">
+          <div className="top">
+            <div className="intro">Get more than a reliable power plant</div>
 
-          <div className="main">
-            Empowering a Sustainable Future with Green Energy Solutions
+            <div className="main">
+              Empowering a Sustainable Future with Green Energy Solutions
+            </div>
+
+            <div className="c-t-a">
+              <Link>Explore Our Products</Link>
+            </div>
           </div>
 
-          <div className="c-t-a">
-            <Link>Explore Our Products</Link>
-          </div>
-        </div>
+          <div className="bottom">
+            <div className="sect-1">
+              <p>PERFECTION IS CREATED, NOT MADE</p>
+              <p> MacFa Nigeria Enterprise is the go-to solution you need</p>
+            </div>
 
-        <div className="bottom">
-          <div className="sect-1">
-            <p>PERFECTION IS CREATED, NOT MADE</p>
-            <p> MacFa Nigeria Enterprise is the go-to solution you need</p>
-          </div>
-          
-          <div className="socials">
-            <a href="http://" target="_blank" rel="noopener noreferrer">
-              <FaInstagram className="icon" />
-            </a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">
-              <FaFacebookF className="icon" />
-            </a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">
-              <FaWhatsapp className="icon" />
-            </a>
+            <div className="socials">
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <FaInstagram className="icon" />
+              </a>
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <FaFacebookF className="icon" />
+              </a>
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <FaWhatsapp className="icon" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
