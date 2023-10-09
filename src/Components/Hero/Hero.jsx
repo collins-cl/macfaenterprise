c8898fe1a8bd2bae239a59f51c381607dbc96e36
Collins-cl <collinsolads@gmail.com>
2023-10-09 23:27:37 +0100

    fix: hero-image

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,7 +5,21 @@ import { Link } from "react-router-dom";
 import { FaInstagram, FaFacebookF, FaWhatsapp } from "react-icons/fa";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div className="hero-container">
+      <div className="wrapper">
+        <div className="middle-contents">
+          <div className="intro">GET MORE THAN A CLEAN ENERGY SOURCE</div>
+
+          <div className="title">
+            Empowering a Sustainable Future with, MacFa Nigeria Enterprise
+          </div>
+
+          <div className="c-t-a">See our products</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "../Home/Home.scss";
 import Navbar from "../../Components/Navbar/Navbar";
+import Hero from "../../Components/Hero/Hero";
 
 const Home = () => {
   return (
     <div className="home">
       <div className="hero">
         <Navbar />
-        
+
+        <div className="hero-contents">
+          <Hero />
+        </div>
       </div>
       <p>scsc</p>
       <p>scsc</p>
