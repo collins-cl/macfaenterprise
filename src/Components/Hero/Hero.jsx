85cbc57bfe51fcd7804ddff55778aad97cb1c156
Collins-cl <collinsolads@gmail.com>
2023-10-10 00:45:46 +0100

    fix: hero-image

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -17,6 +17,31 @@ const Hero = () => {
 
           <div className="c-t-a">See our products</div>
         </div>
+
+        <div className="bottom-contents">
+          <div className="left">
+            <div className="box1">PERFECTION IS CREATED, NOT MADE</div>
+
+            <div className="box1">
+              MACFA NIGERIA ENTERPRISE HAS BEEN RUNNING FOR 10 YEARS AND
+              COUNTING
+            </div>
+          </div>
+
+          <div className="right">
+            <div className="socials">
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <FaFacebookF className="icon" />
+              </a>
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <FaInstagram className="icon" />
+              </a>
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <FaWhatsapp className="icon" />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "../Home/Home.scss";
 import Navbar from "../../Components/Navbar/Navbar";
 import Hero from "../../Components/Hero/Hero";
+import cat1 from "../../assets/cat1.jpeg";
+import cat2 from "../../assets/cat2.jpg";
+import man1 from "../../assets/man1.jpg";
 
 const Home = () => {
   return (
@@ -11,6 +14,51 @@ const Home = () => {
 
         <div className="hero-contents">
           <Hero />
+        </div>
+      </div>
+
+      <div className="about">
+        <div className="left">
+          <div className="head">About Company</div>
+
+          <h2>your trusted electrical energy equipments supplier</h2>
+
+          <div className="brief-info">
+            Welcome to MacFa Nigeria Enterprise, your premier source AVr/KvA
+            generators and transformer solutions that powers a sustainable
+            future.
+            <p>
+              <span>Our commitment: </span> At MacFa, we are more than just a
+              supplier, we are pioneers in the world of AVr transformer. Our
+              mission is to deliver cutting edge transformer solutions and this
+              sets us at an advantage from our competitors. Being the only
+              verified source for the import of AVR transformers into Africa,
+              our flexibility and ability to offer genuine products is worth
+              working with us.
+            </p>
+          </div>
+        </div>
+
+        <div className="right-imgs">
+          <div className="pic1">
+            <img
+              src={man1}
+              width={300}
+              alt="an engineer of macfa generator services"
+            />
+          </div>
+
+          <div className="date">
+            since <span>2007</span>
+          </div>
+
+          <div className="pic2">
+            <img
+              src={cat2}
+              width={300}
+              alt="a generator that macfa services offer"
+            />
+          </div>
         </div>
       </div>
       <p>scsc</p>
